31d120be91fbb2019b4d0abdad30db1680e54ef4
Marius G <gasselingmarius@gmail.com>
2023-01-12 21:41:59 +0100

    commit

--- humanCopyPasteSimulator.ino
+++ humanCopyPasteSimulator.ino
@@ -1,48 +1,52 @@
-/**
- * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
- */
 #include <BleKeyboard.h>
 #include <string.h>
 
 BleKeyboard bleKeyboard;
+int pushDelay= 2000;
 
 void setup() {
   Serial.begin(115200);
   bleKeyboard.begin();
-  Serial.println("Serial & ble init");
+  Serial.print("\tGib Ihm\n");
 }
 
 void loop() {
-    pushKeystroke(getUserSerialInput()); 
-    while (true) {delay(99999);}
+    String s= getUserSerialInput();
+    delay(pushDelay);
+    pushKeystroke(s); 
 }
 
 String getUserSerialInput() {
     String input;
-    while(!Serial.available()){
-        Serial.print("\nPaste Keystroke to push over: ");
-        delay(5000);
-    }
-    Serial.flush();
+    Serial.print("\nPaste Keystroke to push over");
+    while(!Serial.available()){Serial.print(".");delay(1000);}
     input = Serial.readStringUntil('\n');
     input = input.substring(0,input.length());
-    Serial.println("User will push: " + input);
-    Serial.println("            to: BLE Keyboard");
+    Serial.println("\npush:\t" + input);
     return input;
 }
 
 void pushKeystroke(String pushMessage) {
-    while(!bleKeyboard.isConnected()) {
-        Serial.println("BLE connection waiting 5 seconds...");
-        delay(5000);
-    }
-    int str_len = pushMessage.length() + 1; 
+    Serial.print("  to:\tBLE Keyboard");
+    while(!bleKeyboard.isConnected()) {Serial.print(".");delay(1000);}
+    int str_len = pushMessage.length(); 
     for (int i= 0; i < str_len; i++) {
         bleKeyboard.write(pushMessage[i]);
-        Serial.println("execute: "+ pushMessage[i]);
-        delay(100);
+        Serial.println((char) pushMessage[i]);
+        delay(50);
     }
     Serial.println("\nKEYSTROKE EXECUTED");
     return;
+}
 
+void checkInput(String s) {
+    if (s.equals("setDelay")) {
+        Serial.print("setDelay");
+        while(!Serial.available()){Serial.print(".");delay(1000);}
+        String input = Serial.readStringUntil('\n');
+        input = input.substring(0,input.length());
+        pushDelay= input.toInt();
+        Serial.print("\tsetDelay to: " + pushDelay);
+        return;
+    }
 }
