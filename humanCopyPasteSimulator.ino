4d43c0058e0b0a61977ad226f7da71c7993ca9f9
Marius G <gasselingmarius@gmail.com>
2023-01-15 00:21:51 +0100

    cleanUp

--- humanCopyPasteSimulator.ino
+++ humanCopyPasteSimulator.ino
@@ -1,65 +1,56 @@
 #include <BleKeyboard.h>
 #include <string.h>
 
-BleKeyboard bleKeyboard;
-int pushDelay= 2000; // zwischen eingabe & ausgabe
+BleKeyboard bleKeyboard("HumanTrashSim", "419", 100);
+int pushDelay= 2000, typeDelayy= 50;
+String data;
+bool changeParameters= false;
 
 void setup() {
-  Serial.begin(115200);
-  bleKeyboard.begin();
+    Serial.begin(115200);
+    while(!Serial);
+    bleKeyboard.begin();
 }
 
 void loop() {
-    int n= pushDelay;
-    String s= getUserSerialInput();
-    Serial.println(s);
-    checkInput(s);
-    if (pushDelay== n) { // wird übersprungen 
-        delay(pushDelay); // wenn Delay neu
-        pushKeystroke(s); // gesetzt wurde
+    checkBluetoothConnection();
+    data= getUserSerialInput();
+    // Entwerfe Methode zum modifizieren der 
+    // Parameter zur Laufzeit
+    if (!changeParameters) { 
+        delay(pushDelay); 
+        charSwap(); 
+        for (int i= 0; i < data.length(); i++) {
+            bleKeyboard.write(data[i]);
+            delay(typeDelayy);
+        }
+        data= "";
     }
+    Serial.print("\nready for next execute");
 }
 
 String getUserSerialInput() {
-    String input;
+    String s;
     while(!Serial.available()){Serial.print(".");delay(3000);}
-    input = Serial.readStringUntil('\n');
-    input = input.substring(0,input.length());
-    return input;
+    s = Serial.readStringUntil('\n');
+    s = s.substring(0,s.length());
+    return s;
 }
 
-void pushKeystroke(String pushMessage) {
-    Serial.println("\npush:\t" + pushMessage);
-    while(!bleKeyboard.isConnected()) {Serial.print(".");delay(3000);}
-    Serial.print("\n     \t");
-    for (int i= 0; i< pushMessage.length(); i++) { // z & y char swap
-        if ((char) pushMessage[i] == 'y') {
-            pushMessage[i]= 'z';
-        } else if ((char) pushMessage[i] == 'Y') {
-            pushMessage[i]= 'Z';
-        } else if ((char) pushMessage[i] == 'z') {
-            pushMessage[i]= 'y';
-        } else if ((char) pushMessage[i] == 'Z') {
-            pushMessage[i]= 'Y';
-        }
+void charSwap() { // y & z upper/lower case
+    for (int i= 0; i< data.length(); i++) { 
+        if (data[i] == 'y') {data[i]= 'z';
+        } else if (data[i] == 'Y') {data[i]= 'Z';
+        } else if (data[i] == 'z') {data[i]= 'y';
+        } else if (data[i] == 'Z') {data[i]= 'Y';}
     }
-    for (int i= 0; i < pushMessage.length(); i++) { // schleife pusht ein char nach dem anderen
-        bleKeyboard.write(pushMessage[i]);
-        Serial.print(pushMessage[i]);
-        delay(50);
-    }
-    Serial.println("\nKEYSTROKE EXECUTED\n");
-    return;
 }
 
-void checkInput(String s) {
-    if (s.equals("setDelay")) {
-        Serial.print("\nsetDelay");
-        while(!Serial.available()){Serial.print(".");delay(3000);}
-        String input = Serial.readStringUntil('\n');
-        input = input.substring(0,input.length());
-        pushDelay= input.toInt();
-        Serial.print("\tsetDelay to: " + pushDelay);
-        return;
+void checkBluetoothConnection() {
+    if(!bleKeyboard.isConnected()){
+        Serial.print("wait for connection");
+    }
+    while(!bleKeyboard.isConnected()) {
+        Serial.print(".");delay(3000);
     }
 }
